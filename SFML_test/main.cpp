08f90f0cbf068facc3c65d2f1ba9de4887f1db32
TheChromeBookGod <graysonf2017@gmail.com>
2025-04-15 17:14:11 -0700

    Made window open to fullscreen and esc close window

--- SFML_test/main.cpp
+++ SFML_test/main.cpp
@@ -1,13 +1,21 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    //sf::Window App
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!", sf::State::Fullscreen);
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+    shape.setFillColor(sf::Color::Blue);
+    window.setFramerateLimit(60); // sets max frame rate to 60fps
 
     while (window.isOpen())
     {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) {
+            window.close();
+        }
+
+
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
@@ -17,5 +25,8 @@ int main()
         window.clear();
         window.draw(shape);
         window.display();
+
+
+        shape.move({ -1.f, 0.f });
     }
 }
