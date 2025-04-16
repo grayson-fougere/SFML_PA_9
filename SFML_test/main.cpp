90a4b27ded9abf0f57e6977b221c4f67358d2689
Huskiefusion <52263805+Huskiefusion@users.noreply.github.com>
2025-04-16 14:19:43 -0700

    Extracted collision functions to CollisionHelper class

--- SFML_test/main.cpp
+++ SFML_test/main.cpp
@@ -1,24 +1,11 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/System.hpp>
 #include <iostream>
 #include <vector>
 #include "DebugCircle.hpp"
+#include "CollisionHelper.hpp"
 
 using std::cout;
 using std::endl;
 
-std::ostream& operator<<(std::ostream& lhs, const sf::Vector2f& rhs) {
-    lhs << "<" << rhs.x << ", " << rhs.y << ">" << endl;
-    return lhs;
-}
-
-//std::vector<sf::Vector2f> findAllIntersections(sf::CircleShape a, sf::RectangleShape b);
-//std::vector<sf::Vector2f> findAllIntersections(sf::CircleShape a, sf::ConvexShape b);
-std::vector<sf::Vector2f> findAllIntersections(sf::Shape& a, sf::Shape& b);
-std::optional<sf::Vector2f> findIntersection(const float& x1, const float& y1, const float& x2, const float& y2, const float& x3, const float& y3, const float& x4, const float& y4);
-
-bool isInRange(const float& n, const float& bound1, const float& bound2);
 
 int main()
 {
@@ -67,7 +54,7 @@ int main()
 
         std::optional intersect = shape.getGlobalBounds().findIntersection(shape2.getGlobalBounds());
         if (intersect) {
-            std::vector<sf::Vector2f> intersects = findAllIntersections(shape, shape2);
+            std::vector<sf::Vector2f> intersects = CollisionHelper::findAllIntersections(shape, shape2);
             std::cout << "intersect size: " << intersects.size() << std::endl;
             //colcirc2.setPosition(shape.getTransform().transformPoint( shape.getPoint(shape.getPointCount()-1)) );
             if (intersects.size()!=0)colcirc1.setPosition(intersects[0]);
@@ -105,55 +92,3 @@ int main()
 }
 
 
-bool isInRange(const float &n, const float &bound1, const float &bound2) {
-    if (bound2 >= bound1) {
-        return n >= bound1 && n <= bound2;
-    }
-    else {
-        return n <= bound1 && n >= bound2;
-    }
-}
-
-//https://en.wikipedia.org/wiki/Line–line_intersection
-std::optional<sf::Vector2f> findIntersection(const float &x1, const float& y1, const float& x2, const float& y2, 
-    const float& x3, const float& y3, const float& x4, const float& y4) {
-    float denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4), // denominator
-        numerX = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4), // numerator of the X component of the intersection
-        numerY = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4); // numerator of the Y component of the intersection
-    if (denom != 0) {
-        sf::Vector2f i(numerX / denom, numerY / denom);
-        if (isInRange(i.x, x1, x2) && isInRange(i.x, x3, x4) && isInRange(i.y, y1, y2) && isInRange(i.y, y3, y4))
-            return i;
-        else return std::nullopt;
-    }
-    else {
-        return std::nullopt;
-    }
-}
-
-// Perhaps this should return a reference to help with performance
-std::vector<sf::Vector2f> findAllIntersections(sf::Shape& a, sf::Shape& b)
-{
-    std::vector<sf::Vector2f> intersects;
-    for (int i = a.getPointCount() - 1; i >= 0; i--) {
-        float x1 = a.getTransform().transformPoint(a.getPoint(i)).x, y1 = a.getTransform().transformPoint(a.getPoint(i)).y,
-            x2 = a.getTransform().transformPoint(a.getPoint(i - 1)).x, y2 = a.getTransform().transformPoint(a.getPoint(i - 1)).y; // Start and end points of this edge
-        if (i - 1 == -1) {
-            x2 = a.getTransform().transformPoint(a.getPoint(a.getPointCount() - 1)).x, y2 = a.getTransform().transformPoint(a.getPoint(a.getPointCount() - 1)).y;
-        }
-        for (int j = b.getPointCount() - 1; j >= 0; j--) {
-            float x3 = b.getTransform().transformPoint(b.getPoint(j)).x, y3 = b.getTransform().transformPoint(b.getPoint(j)).y,
-                x4 = b.getTransform().transformPoint(b.getPoint(j - 1)).x, y4 = b.getTransform().transformPoint(b.getPoint(j - 1)).y;
-            if (j - 1 == -1) {
-                x4 = b.getTransform().transformPoint(b.getPoint(b.getPointCount() - 1)).x, y4 = b.getTransform().transformPoint(b.getPoint(b.getPointCount() - 1)).y;
-            }
-            std::optional v = findIntersection(x1, y1, x2, y2, x3, y3, x4, y4);
-            if (v) {
-                intersects.push_back(v.value());
-                cout << v.value() << endl;
-            }
-        }
-        //std::cout << i << std::endl;
-    }
-    return intersects;
-}
