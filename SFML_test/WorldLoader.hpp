604a169f52f032189274cb8597b63232f30c80d8
Logan Meyers <theterrarian03@gmail.com>
2025-04-22 13:15:27 -0700

    Added idea of what characters should mean and do what in a level file

--- SFML_test/WorldLoader.hpp
+++ SFML_test/WorldLoader.hpp
@@ -3,6 +3,28 @@
 #include "PlayerBall.hpp"
 #include "Camera.hpp"
 
+/*
+What the characters mean in a level file:
+
+l [LEFT EDGE]: clamp camera from moving past that left
+r [RIGHT EDGE]: clamp camera from movign past that right
+0 [AIR]: no block here
+1 [GROUND]: filler for world, has collisions but no special properties
+2 [FLOOR]: has collision, allows player to reset jump here
+s [SPIKE]: dangerous spike, kills player. physical collision not necessary
+c [COIN]: collectable coin, no physical collision
+f [FLAG]: win condition flag
+	has next level to load defined at the bottom of the file. e.x.:
+		f "levelSelect.txt"
+p [PLAYER]: spawn position for player
+z# [LOAD #]: loads new level, defined at bottom of file with z# "newLevel.txt"
+	level loads are stored into a vector, so start counting at 0. e.x.:
+		z1 "level1.txt"
+		z2 "level2.txt"
+		z3 "BOSSFIGHT.txt"
+		z4 "thepit.html"
+*/
+
 class WorldLoader {
 	static void loadLevel(std::vector<Collidable>& worldObjects, PlayerBall& player, sf::RenderWindow& window, Camera& camera) {
 
