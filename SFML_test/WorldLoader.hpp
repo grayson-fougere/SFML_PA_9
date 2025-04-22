ecdacb125bd0e903834c2c3f25c2d22f25fdde38
Logan Meyers <theterrarian03@gmail.com>
2025-04-21 20:17:57 -0700

    Started to work on a world loader

---
+++ SFML_test/WorldLoader.hpp
@@ -0,0 +1,12 @@
+#include <SFML/Graphics.hpp>
+#include "Collidable.hpp"
+#include "PlayerBall.hpp"
+#include "Camera.hpp"
+
+class WorldLoader {
+	static void loadLevel(std::vector<Collidable>& worldObjects, PlayerBall& player, sf::RenderWindow& window, Camera& camera) {
+
+	}
+
+
+};
