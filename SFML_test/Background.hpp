3a5f6e8e6262c1a5e507806781992545238f3aee
Logan Meyers <theterrarian03@gmail.com>
2025-04-23 18:06:03 -0700

    Betterize background

--- SFML_test/Background.hpp
+++ SFML_test/Background.hpp
@@ -2,22 +2,17 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <memory>
+#include "Camera.hpp"
+
 class Background {
 public:
     Background();
-    void draw(sf::RenderWindow& window);
+    void draw(sf::RenderWindow& window, Camera& viewCam);
     void resize(sf::Vector2u windowSize);
     void scroll(float offset);
 
-    sf::Texture farTex, midTex, nearTex;
+    sf::Texture bg_img;
 
 private:
-    struct Layer {
-        Layer(sf::Texture& tex, float speed);
-        sf::Texture& texture;
-        sf::Sprite sprite;
-        float scrollSpeed;
-    };
-
-    std::vector<Layer> layers;
+    std::vector<sf::Sprite*> images;
 };
